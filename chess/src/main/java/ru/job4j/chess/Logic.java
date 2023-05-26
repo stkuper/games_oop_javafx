a8c416a6fdc8bfaf023ca1c735b2432d9de19672
stkuper <stklipper@gmail.com>
2023-05-26 21:59:36 +0300

    3. Каркас шахматной доски [#792]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -3,6 +3,7 @@ package ru.job4j.chess;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 import java.util.Arrays;
+import java.util.Objects;
 
 public final class Logic {
     private final Figure[] figures = new Figure[32];
@@ -23,8 +24,7 @@ public final class Logic {
     private boolean free(Cell[] steps) throws OccupiedCellException {
         for (Figure figure : figures) {
             for (Cell step : steps) {
-                if (figure.position().getX() == step.getX()
-                        && figure.position().getY() == step.getY()) {
+                if (figure.position().equals(step)) {
                     throw new OccupiedCellException(String.format("Cell %s is occupied", step));
                 }
             }
@@ -45,5 +45,24 @@ public final class Logic {
             }
         }
         throw new FigureNotFoundException("Figure not found on the board.");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Logic logic = (Logic) o;
+        return index == logic.index && Arrays.equals(figures, logic.figures);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(index);
+        result = 31 * result + Arrays.hashCode(figures);
+        return result;
     }
 }
